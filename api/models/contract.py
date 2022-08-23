5fe562c0d19f407e34274d636940e94a3cf486f6
DRG104 <ph_kyle104@yahoo.com>
2022-08-23 00:08:26 -0500

    Migrations are messy, contracts finished

--- api/models/contract.py
+++ api/models/contract.py
@@ -1,14 +1,27 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Contract(models.Model):
+
+  class JobType(models.TextChoices):
+    UI = 'UI', _('UI')
+    UX = 'UX', _('UX')
+    FRONTEND = 'FE', _('Front End')
+    BACKEND = 'BE', _('Back End')
+    FULLSTACK = 'FS', _('Full Stack')
+    REFACTOR = 'RE', _('Refactor')
+
+  
   # define fields
   # https://docs.djangoproject.com/en/3.0/ref/models/fields/
   title = models.CharField(max_length=100)
   description = models.TextField(max_length=500)
   deadline = models.DateTimeField(auto_now=False)
+  jobtype = models.CharField(max_length=2, choices=JobType.choices, default=None)
   tags = models.CharField(max_length=100)
+  price = models.CharField(max_length=8)
   updated_at = models.DateTimeField(auto_now=True)
   created_at = models.DateTimeField(auto_now_add=True)
   owner = models.ForeignKey(
@@ -27,6 +40,8 @@ class Contract(models.Model):
         'title': self.title,
         'description': self.description,
         'deadline': self.deadline,
+        'Job Type': self.jobtype,
+        'price': self.price,
         'tags': self.tags,
         'owner': self.owner,
     }
