11f7cd18ea981d7c489619975f77328e396f5dc9
DRG104 <ph_kyle104@yahoo.com>
2022-08-22 22:45:01 -0500

    Templated contract from mangos

--- api/serializers.py
+++ api/serializers.py
@@ -2,12 +2,18 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from .models.mango import Mango
+from .models.contract import Contract
 from .models.user import User
 
 class MangoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mango
         fields = ('id', 'name', 'color', 'ripe', 'owner')
+
+class ContractSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contract
+        fields = ('id', 'title', 'description', 'deadline', 'tags', 'owner')
 
 class UserSerializer(serializers.ModelSerializer):
     # This model serializer will be used for User creation
--- api/views/contract_views.py
+++ api/views/contract_views.py
@@ -4,76 +4,76 @@ from rest_framework.exceptions import PermissionDenied
 from rest_framework import generics, status
 from django.shortcuts import get_object_or_404
 
-from ..models.mango import Mango
-from ..serializers import MangoSerializer
+from ..models.contract import Contract
+from ..serializers import ContractSerializer
 
 
 # Create your views here.
-class Mangos(generics.ListCreateAPIView):
+class Contracts(generics.ListCreateAPIView):
     permission_classes=(IsAuthenticated,)
-    serializer_class = MangoSerializer
+    serializer_class = ContractSerializer
     def get(self, request):
         """Index request"""
-        # Get all the mangos:
-        # mangos = Mango.objects.all()
-        # Filter the mangos by owner, so you can only see your owned mangos
-        mangos = Mango.objects.filter(owner=request.user.id)
+        # Get all the contracts:
+        # contracts = Contract.objects.all()
+        # Filter the contracts by owner, so you can only see your owned contracts
+        contracts = Contract.objects.filter(owner=request.user.id)
         # Run the data through the serializer
-        data = MangoSerializer(mangos, many=True).data
-        return Response({ 'mangos': data })
+        data = ContractSerializer(contracts, many=True).data
+        return Response({ 'contracts': data })
 
     def post(self, request):
         """Create request"""
         # Add user to request data object
-        request.data['mango']['owner'] = request.user.id
-        # Serialize/create mango
-        mango = MangoSerializer(data=request.data['mango'])
-        # If the mango data is valid according to our serializer...
-        if mango.is_valid():
-            # Save the created mango & send a response
-            mango.save()
-            return Response({ 'mango': mango.data }, status=status.HTTP_201_CREATED)
+        request.data['contract']['owner'] = request.user.id
+        # Serialize/create contract
+        contract = ContractSerializer(data=request.data['contract'])
+        # If the contract data is valid according to our serializer...
+        if contract.is_valid():
+            # Save the created contract & send a response
+            contract.save()
+            return Response({ 'contract': contract.data }, status=status.HTTP_201_CREATED)
         # If the data is not valid, return a response with the errors
-        return Response(mango.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(contract.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class MangoDetail(generics.RetrieveUpdateDestroyAPIView):
+class ContractDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes=(IsAuthenticated,)
     def get(self, request, pk):
         """Show request"""
-        # Locate the mango to show
-        mango = get_object_or_404(Mango, pk=pk)
-        # Only want to show owned mangos?
-        if request.user != mango.owner:
-            raise PermissionDenied('Unauthorized, you do not own this mango')
+        # Locate the contract to show
+        contract = get_object_or_404(Contract, pk=pk)
+        # Only want to show owned contracts?
+        if request.user != contract.owner:
+            raise PermissionDenied('Unauthorized, you do not own this contract')
 
         # Run the data through the serializer so it's formatted
-        data = MangoSerializer(mango).data
-        return Response({ 'mango': data })
+        data = ContractSerializer(contract).data
+        return Response({ 'contract': data })
 
     def delete(self, request, pk):
         """Delete request"""
-        # Locate mango to delete
-        mango = get_object_or_404(Mango, pk=pk)
-        # Check the mango's owner against the user making this request
-        if request.user != mango.owner:
-            raise PermissionDenied('Unauthorized, you do not own this mango')
-        # Only delete if the user owns the  mango
-        mango.delete()
+        # Locate contract to delete
+        contract = get_object_or_404(Contract, pk=pk)
+        # Check the contract's owner against the user making this request
+        if request.user != contract.owner:
+            raise PermissionDenied('Unauthorized, you do not own this contract')
+        # Only delete if the user owns the  contract
+        contract.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
     def partial_update(self, request, pk):
         """Update Request"""
-        # Locate Mango
-        # get_object_or_404 returns a object representation of our Mango
-        mango = get_object_or_404(Mango, pk=pk)
-        # Check the mango's owner against the user making this request
-        if request.user != mango.owner:
-            raise PermissionDenied('Unauthorized, you do not own this mango')
+        # Locate Contract
+        # get_object_or_404 returns a object representation of our Contract
+        contract = get_object_or_404(Contract, pk=pk)
+        # Check the contract's owner against the user making this request
+        if request.user != contract.owner:
+            raise PermissionDenied('Unauthorized, you do not own this contract')
 
         # Ensure the owner field is set to the current user's ID
-        request.data['mango']['owner'] = request.user.id
+        request.data['contract']['owner'] = request.user.id
         # Validate updates with serializer
-        data = MangoSerializer(mango, data=request.data['mango'], partial=True)
+        data = ContractSerializer(contract, data=request.data['contract'], partial=True)
         if data.is_valid():
             # Save & send a 204 no content
             data.save()
