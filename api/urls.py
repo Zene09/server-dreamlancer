c28bf7d82d1b5f3fec93261cbd3066bb7223c3c4
DRG104 <ph_kyle104@yahoo.com>
2022-08-22 22:52:48 -0500

    Added url patterns, finished views

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views.mango_views import Mangos, MangoDetail
+from .views.contract_views import Contracts, ContractDetail
 from .views.user_views import SignUp, SignIn, SignOut, ChangePassword
 
 
@@ -7,6 +8,8 @@ urlpatterns = [
   	# Restful routing
     path('mangos/', Mangos.as_view(), name='mangos'),
     path('mangos/<int:pk>/', MangoDetail.as_view(), name='mango_detail'),
+    path('contracts/', Contracts.as_view(), name='contracts'),
+    path('contracts/<int:pk>/', ContractDetail.as_view(), name='contract_detail'),
     path('sign-up/', SignUp.as_view(), name='sign-up'),
     path('sign-in/', SignIn.as_view(), name='sign-in'),
     path('sign-out/', SignOut.as_view(), name='sign-out'),
