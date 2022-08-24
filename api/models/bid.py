9856f572d4fbfc4f592ef85e502aa185b5ecc1fa
DRG104 <ph_kyle104@yahoo.com>
2022-08-24 02:03:44 -0500

    Added the rest of bids

--- api/models/bid.py
+++ api/models/bid.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from django.utils.translation import gettext_lazy as _
+# from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Bid(models.Model):
